f4b7c1a690a8bde37da445a944dbc67190f82425
chrissyhunt87 <chrissy.hunt@gmail.com>
2018-08-13 20:06:25 -0400

    Set up routes

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,32 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const Welcome = () => {
+  return (
+    <div>
+      <h1>Welcome!</h1>
+    </div>
+  )
+}
+
+const User = () => {
+  return (
+    <div>
+      <h1>User Settings</h1>
+    </div>
+  )
+}
+
+ReactDOM.render((
+  <Router>
+    <React.Fragment>
+      <Route exact path="/" render={Welcome} />
+      <Route exact path="/news" component={App} />
+      <Route exact path="/account" render={User} />
+    </React.Fragment>
+  </Router>),
+  document.getElementById('root')
+);
 registerServiceWorker();
