8eab10b9cba21f3f508b7f40bd11d2d72ebf1602
chrissyhunt87 <chrissy.hunt@gmail.com>
2018-08-15 07:44:06 -0400

    Building out NewsRequestController

--- app/controllers/concerns/news_request_controller.rb
+++ app/controllers/concerns/news_request_controller.rb
@@ -3,17 +3,28 @@ require 'rest-client'
 class NewsRequestController < ApplicationController
 
   def create
-    @results = RestClient.get('https://newsapi.org/v2/everything?', {params: {
+    @news_results = RestClient.get('https://newsapi.org/v2/everything?', {params: {
       apiKey: my_key,
       q: news_request_params.query,
       sources: sources,
-      from: news_request_params.start_date,
-      to: news_request_params.end_date,
+      from: set_start_date,
+      to: set_end_date,
       sortBy: relevancy
       }})
+      if @news_results
+        render json: format_news_results(@news_results)
+      end
   end
 
   private
+
+  def set_start_date
+    news_request_params.start_date ? news_request_params.start_date : DateTime.now.strftime('%F')
+  end
+
+  def set_end_date
+    news_request_params.end_date ? news_request_params.end_date : DateTime.now.strftime('%F')
+  end
 
   def news_request_params
     params.require(:news_request).permit(:query, :start_date, :end_date)
@@ -24,6 +35,7 @@ class NewsRequestController < ApplicationController
   end
 
   def sources
+    'axios,bloomberg,breitbart-news,buzzfeed,cnn,fox-news,msnbc,national-review,reuters,the-american-conservative,the-huffington-post,the-new-york-times,the-wall-street-journal,the-washington-post,usa-today,the-hill,politico,the-economist,the-washington-times,time'
   end
 
 end
