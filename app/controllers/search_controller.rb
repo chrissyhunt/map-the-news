156555692ed84d4c5ca789836ee87f7d03a24229
chrissyhunt87 <chrissy.hunt@gmail.com>
2018-08-14 20:50:05 -0400

    Built basic Search controller

---
+++ app/controllers/search_controller.rb
@@ -0,0 +1,21 @@
+class SearchController < ApplicationController
+
+  def create
+    @search = Search.new(search_params)
+    if @search.save
+      render json: @search, status: 200
+    end
+  end
+
+  def show
+    @search = Search.find_by(id: params[:id])
+    render json: @search, status: 200
+  end
+
+  def destroy
+    @search = Search.find_by(id: params[:id])
+    @search.deletes
+    render body: nil, status: :no_content
+  end
+
+end
