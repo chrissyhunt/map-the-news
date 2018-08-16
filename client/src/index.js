d3da4675e8643b85f65e1ab25da16b82dcfc84c8
chrissyhunt87 <chrissy.hunt@gmail.com>
2018-08-15 21:27:03 -0400

    Added skeleton NewsItemsReducer, RootReducer, and installed redux-thunk

--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,12 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import rootReducer from './reducers/RootReducer';
+
+const store = createStore(rootReducer, applyMiddleware(thunk));
 
 const Welcome = () => {
   return (
---
+++ client/src/reducers/NewsItemsReducer.js
@@ -0,0 +1,9 @@
+import { combineReducers } from 'redux';
+
+function newsItemsReducer(state = [], action) {
+  switch (action.type) {
+    // fill in stuff
+  }
+}
+
+export default newsItemsReducer;
