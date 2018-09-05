7ff3b8239d24c0f03cd5f3acfc6c95938a10f508
chrissyhunt87 <chrissy.hunt@gmail.com>
2018-09-05 07:15:24 -0400

    Updated row headlines

--- client/src/components/NewsMapDisplay/LoadRows.js
+++ client/src/components/NewsMapDisplay/LoadRows.js
@@ -4,9 +4,9 @@ const LoadRows = (props) => {
   return (
     <React.Fragment>
       <div class="row green-row"><h3>Original Fact Reporting</h3></div>
-      <div class="row yellow-row"><h3>Fair Analysis & Opinion</h3></div>
+      <div class="row yellow-row"><h3>Fact Reporting / Fair Analysis & Opinion</h3></div>
       <div class="row orange-row"><h3>Incomplete Story</h3></div>
-      <div class="row red-row"><h3>Propaganda & Nonsense</h3></div>
+      <div class="row red-row"><h3>Propaganda & Misinformation</h3></div>
     </React.Fragment>
   )
 }
