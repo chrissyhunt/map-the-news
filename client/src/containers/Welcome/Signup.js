a920fc9caf1da5c4e77929927e476e8e9b465ee7
chrissyhunt87 <chrissy.hunt@gmail.com>
2018-09-10 08:00:18 -0400

    Got create account client-side form validation working

--- client/src/containers/Welcome/Signup.js
+++ client/src/containers/Welcome/Signup.js
@@ -13,6 +13,18 @@ class Signup extends Component {
         password: '',
         first_name: '',
         last_name: ''
+      },
+      errors: {
+        email: true,
+        password: true,
+        first_name: true,
+        last_name: false
+      },
+      touched: {
+        email: false,
+        password: false,
+        first_name: false,
+        last_name: false
       }
     }
   }
@@ -39,31 +51,84 @@ class Signup extends Component {
     })
   }
 
+  handleBlur = (event) => {
+    this.setState({
+      touched: {
+        ...this.state.touched,
+        [event.target.name]: true
+      }
+    })
+  }
+
+  validate = (email, password, first_name) => {
+    return {
+      email: !email.match(/^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/g),
+      password: password.length === 0,
+      first_name: first_name.length === 0
+    }
+  }
+
   render() {
+    const errors = this.validate(this.state.userInfo.email, this.state.userInfo.password, this.state.userInfo.first_name);
+    const isEnabled = !Object.keys(errors).some(x => errors[x])
+    const shouldMarkError = (field) => {
+      const hasError = errors[field];
+      const shouldShow = this.state.touched[field];
+      return hasError && shouldShow;
+    }
+
     return (
       <form onSubmit={e => this.handleSubmit(e)}>
         <div className="welcome-half-width tab-left">
           <label>First Name:</label>
-          <input type="text" value={this.state.userInfo.first_name} name="first_name" onChange={e => this.handleChange(e)}/><br />
+          <input
+            className={shouldMarkError('first_name') ? "error" : ""}
+            type="text"
+            value={this.state.userInfo.first_name}
+            name="first_name"
+            onChange={e => this.handleChange(e)}
+            onBlur={e => this.handleBlur(e)}
+          /><br />
         </div>
 
         <div className="welcome-half-width tab-right">
           <label>Last Name:</label>
-          <input type="text" value={this.state.userInfo.last_name} name="last_name" onChange={e => this.handleChange(e)}/><br />
+          <input
+            className={shouldMarkError('last_name') ? "error" : ""}
+            type="text"
+            value={this.state.userInfo.last_name}
+            name="last_name"
+            onChange={e => this.handleChange(e)}
+            onBlur={e => this.handleBlur(e)}
+          /><br />
         </div>
 
         <div className="welcome-full-width">
           <label>Email:</label>
-          <input type="text" value={this.state.userInfo.email} name="email" onChange={e => this.handleChange(e)}/><br />
+          <input
+            className={shouldMarkError('email') ? "error" : ""}
+            type="text"
+            value={this.state.userInfo.email}
+            name="email"
+            onChange={e => this.handleChange(e)}
+            onBlur={e => this.handleBlur(e)}
+          /><br />
         </div>
 
         <div classname="welcome-full-width">
           <label>Password:</label>
-          <input type="password" value={this.state.userInfo.password} name="password" onChange={e => this.handleChange(e)}/><br />
+          <input
+            className={shouldMarkError('password') ? "error" : ""}
+            type="password"
+            value={this.state.userInfo.password}
+            name="password"
+            onChange={e => this.handleChange(e)}
+            onBlur={e => this.handleBlur(e)}
+          /><br />
         </div>
 
         <div className="welcome-full-width">
-          <input type="submit" value="Create Account"/>
+          <input type="submit" value="Create Account" disabled={!isEnabled}/>
         </div>
       </form>
     );
