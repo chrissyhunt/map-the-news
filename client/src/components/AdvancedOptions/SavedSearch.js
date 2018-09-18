7578e5938a8034c815c5b97ece124ff340a03e05
chrissyhunt87 <chrissy.hunt@gmail.com>
2018-09-18 14:30:33 -0400

    Converted SavedSearch to stateful container component

--- client/src/components/AdvancedOptions/SavedSearch.js
+++ client/src/components/AdvancedOptions/SavedSearch.js
@@ -1,13 +1,17 @@
 import React from 'react';
+import { Component } from 'react';
 import moment from 'moment';
 
-const SavedSearch = (props) => {
-  const startDate = moment(props.startDate).format("M/D/YYYY")
-  const endDate = moment(props.endDate).format("M/D/YYYY")
-  const dateRange = startDate === endDate ? startDate : `${startDate} - ${endDate}`
-  return (
-    <p className="list"><input type="button" value="x" onClick={props.deleteSearch} name={props.id} /> <a role="button" name={props.id} onClick={props.loadSavedSearch}>{props.query}</a> <span className="date-range">{dateRange}</span></p>
-  )
+class SavedSearch extends Component {
+  render() {
+    const startDate = moment(this.props.startDate).format("M/D/YYYY")
+    const endDate = moment(this.props.endDate).format("M/D/YYYY")
+    const dateRange = startDate === endDate ? startDate : `${startDate} - ${endDate}`
+
+    return (
+      <p className="list"><input type="button" value="x" onClick={this.props.deleteSearch} name={this.props.id} /> <a role="button" name={this.props.id} onClick={this.props.loadSavedSearch}>{this.props.query}</a> <span className="date-range">{dateRange}</span></p>
+    )
+  }
 }
 
 export default SavedSearch;
