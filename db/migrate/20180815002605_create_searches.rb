c70ae00fb8c0d147d2f86c6dc180334d59baf18d
chrissyhunt87 <chrissy.hunt@gmail.com>
2018-09-30 18:12:02 -0400

    Created development/test dbs, migrated, started server and checked everything working ok in development

--- db/migrate/20180815002605_create_searches.rb
+++ db/migrate/20180815002605_create_searches.rb
@@ -1,7 +1,7 @@
 class CreateSearches < ActiveRecord::Migration[5.1]
   def change
     create_table :searches do |t|
-      t.string :query
+      t.string :q
       t.date :start_date
       t.date :end_date
       t.integer :user_id
